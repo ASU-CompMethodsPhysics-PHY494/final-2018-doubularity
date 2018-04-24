c24bb6b993fd33b685da3565ee0a9fdb1ba2a930
Reid Baker <rabaker7@asu.edu>
2018-04-23 19:55:52 -0700

    About to add shapes for collision objects

--- Work/path1.py
+++ Work/path1.py
@@ -9,7 +9,20 @@ import numpy as np
 # m -> mass of the black hole
 # h -> time step
 
-def tracePath(r0,v0,L,M,m,h = 0.01):
+maxDist = 100
+
+def collided():
+    return False
+
+def killConditionsMet(r,m):
+    if r <= 3*m:
+        return True
+    elif r > masDist:
+        return True
+    if collided() == True:
+        return True
+
+def tracePath(r0,v0,L,M,m,h = 0.01,t = 0):
     y = r0
     y.append(v0)
     y.append(M)
@@ -22,7 +35,16 @@ def tracePath(r0,v0,L,M,m,h = 0.01):
         M^dot = np.cos(y[1])/np.sin(y[1])**3 * (y[0]-2*m)/y[0]**3) * L**2 #The change in angular momentum about the theta angle
 
         theta^dot = (y[0]-2*m)/y[0]**3*y[4]
-        phi^dot = (y[0]-2*m)/y[0]**3*y[4]/np.sin(y[1])**2
+        phi^dot = (y[0]-2*m)/y[0]**3*y[4]/np.sin(y[1])**2 * L
 
         return [y[3],theta^dot,phi^dot,ar,M^dot]
+
+    positions = []
+    while killConditionsMet(r,m) == False:
+        positions.append(t,y[0],y[1],y[2],y[3])
+        y[:] = ode.velocity_verlet(y,f,t,h)
+        t += h
+
+    return np.array(positions)
+
         
