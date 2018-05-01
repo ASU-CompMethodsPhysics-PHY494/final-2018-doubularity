9d9c11668ff50bbff1ed8b5c1e80f1cb201b2289
Tyler Cox <tacox5@asu.edu>
2018-05-01 16:37:45 -0700

    Update Ray Tracer

--- Submission/py/ray_tracing.py
+++ Submission/py/ray_tracing.py
@@ -21,10 +21,9 @@ class Plane:
       Position of the image in spherical coordinates
 
     '''
-    def __init__(self,path='../figures/sagrada-familia.jpg',r=[10,0,0],normal=[1,0,0],scale=10):
+    def __init__(self,path='../figures/checker_board.jpg',r=[1,0,0],scale=3):
         self.path = path
         self.r = np.array(r)
-        self.normal = np.array(normal)
         self.img = misc.imread(self.path)/255
         ratio = self.img.shape[0]/self.img.shape[1]
         w = scale
@@ -59,14 +58,13 @@ def trace_ray(pos,theta,phi,h=0.1):
     velocity = np.array([v_x,v_y,v_z])
     point = np.array(pos)
     h2 = sqrnorm(np.cross(point,velocity))
-
-    while np.linalg.norm(point) <= 15:
-        y = np.zeros(6)
+    color = np.zeros(3)
+    y = np.zeros(6)
+    while (sqrnorm(point)) <= 100:
         y[0:3] = point
         y[3:6] = velocity
         increment = rk4(y,RK4f,h2,h)
-        color = np.zeros(3)
-        if np.linalg.norm(point+increment[0:3]) < 0.01:
+        if sqrnorm(point+increment[0:3]) < 0.01:
             break
         if ((obj.p1[0] <= point[1]+increment[1] <= obj.p2[0]) and
              (obj.p1[1] <= point[2]+increment[2] <= obj.p2[1]) and
@@ -78,55 +76,48 @@ def trace_ray(pos,theta,phi,h=0.1):
     return (color)
 
 
-def ray_cast(w=640,h=480,cam=[-10.0,0.0,0.0],quad=None):
+def ray_cast(w=160,h=90,cam=[-10.0,0,0,0.0]):
     FOV_w=np.deg2rad(40)
     FOV_h=np.deg2rad(30)
     img = np.zeros((h,w,3))
     pix = w*h
     count = 0
-    if quad == 1:
-        t_ang = np.linspace(0,FOV_h/4,h/2)
-        p_ang = np.linspace(0,FOV_w/4,w/2)
-    elif quad == 2:
-        t_ang = np.linspace(0,FOV_h/4,h/2)
-        p_ang = np.linspace(-FOV_w/4,0,w/2)
-    elif quad == 3:
-        t_ang = np.linspace(-FOV_h/4,0,h/2)
-        p_ang = np.linspace(0,FOV_w/4,w/2)
-    elif quad == 4:
-        t_ang = np.linspace(-FOV_h/4,0,h/2)
-        p_ang = np.linspace(-FOV_w/4,0,w/2)
-    else:
-        t_ang = np.linspace(-FOV_h/2,FOV_h/2,h)
-        p_ang = np.linspace(-FOV_w/2,FOV_w/2,w)
+    t_ang = np.linspace(-FOV_h/2,FOV_h/2,h)
+    p_ang = np.linspace(-FOV_w/2,FOV_w/2,w)
     for i,phi in enumerate(t_ang):
         for j,theta in enumerate(p_ang):
             color = trace_ray(cam,theta,phi)
             img[i][j] = color
-            if count/pix*100%5 == 0:
-                print ('{}%...'.format(count/pix*100))
-            count += 1
+            print (i,j)
     print ('100%\nDone.')
     return (img)
-    '''
-    plt.imshow(img,interpolation='nearest')
-    plt.title('Single Image Ray Tracing')
-    print ('Saving Image')
-    plt.show()
-    '''
 
-from multiprocessing.dummy import Pool as ThreadPool
+import time
 
 obj = Plane()
 CAMPOS = [-10.0,0.0,0.0]
 FOV_w = np.deg2rad(40)
 FOV_h = np.deg2rad(30)
-RES = [16,9]
-ray_cast(w=RES[0],h=RES[1])
+RES = np.array([640,480])
+start = time.time()
+img = ray_cast(RES[0],RES[1],CAMPOS)
+print ('Time: {} s'.format(time.time()-start))
+plt.imshow(img,interpolation='nearest')
+plt.savefig('tracing_640_480.png',dpi=300)
+
+'''
 
 in_vals = [[RES,CAMPOS,1],[RES,CAMPOS,2],
             [RES,CAMPOS,3],[RES,CAMPOS,4]]
 
 pool = ThreadPool(4)
-results = zip(*pool.map(ray_cast, in_vals))
-print (results)
+results = pool.map(ray_cast, in_vals)
+print (np.array(results).shape)
+top = np.concatenate((results[3], results[1]), axis=0)
+bottom = np.concatenate((results[2], results[0]), axis=0)
+img = np.concatenate((top,bottom),axis=1)
+
+print (top.shape,bottom.shape)
+plt.imshow(img,interpolation='nearest')
+plt.show()
+'''
